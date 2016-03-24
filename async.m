9481bf0d9d1f6b96bb6f8ce8e6af0c011991756c
YangMengQi <597337796@qq.com>
2016-03-24 20:48:29 +0800

    *修改async.m内容

--- async.m
+++ async.m
@@ -3,10 +3,6 @@
 //
 // IDECodeSnippetCompletionPrefix: async
 // IDECodeSnippetCompletionScopes: [Function or Method]
-// IDECodeSnippetIdentifier: D6498A70-212B-426D-9DD5-C80EAE4E08FC
-// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
-// IDECodeSnippetUserSnippet: 1
-
 dispatch_async(dispatch_get_global_queue(<#dispatch_queue_priority_t priority#>, <#unsigned long flags#>), ^(void) {
     <#code#>
 
